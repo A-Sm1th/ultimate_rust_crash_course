4da800d749a19abfc568bab76bf8fa468aeee3eb
A-Sm1th <ronald.fortmann@gmail.com>
2022-09-29 17:25:29 +0200

    Exercises

---
+++ exercise/e_ownership_references/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn inspect (arg: &String) {
+    if arg.ends_with("s") { println!("plural") } else { println!("singular") }
+}
+
+pub fn change (arg: &mut String) {
+    if !arg.ends_with("s"){ arg.push_str("s") }
+}
+
+pub fn eat (arg: String) -> bool {
+    arg.starts_with("b") & arg.contains("a")
+}
+
+pub fn bedazzle (arg: &mut String) -> String {
+    *arg = String::from("sparkly");
+    return arg.to_string();
+}
